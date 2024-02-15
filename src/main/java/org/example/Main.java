d3f4b68d6d85cd816720ddab7c2f52783dc9df26
Nuris <230445@astanait.edu.kz>
2024-02-15 21:53:03 +0600

    created  CONNECT CLASS and changed main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,47 +9,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/simpledb";
+        String username = "postgres";
+        String password = "123456";
 
-        String conString = "jdbc:postgresql://localhost:5432/simpledb";
-        Connection con = null;
-        ResultSet rs = null;
-        Statement statement = null;
-        ArrayList<User> users = new ArrayList<User>();
-        try {
-            Class.forName("org.postgresql.Driver");
-            con = DriverManager.getConnection(conString, "postgres", "123456");
-            statement = con.createStatement();
-            rs = statement.executeQuery("SELECT id, name, surname, gender, age FROM users ORDER BY id ");
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                Boolean gender = rs.getBoolean("gender");
-                int age = rs.getInt("age");
-
-                User user = new User(id,age, name, surname, gender);
-                users.add(user);
-            }
-        }
-        catch (SQLException | ClassNotFoundException e){
-            System.out.println("Connection error " + e.getMessage());
-        }
-        finally {
-            try{
-                if (rs != null) {
-                    rs.close();
-                }
-                if (statement != null) {
-                    statement.close();
-                }
-                if(con != null) {
-                    con.close();
-                }
-            }
-            catch(SQLException e) {
-                System.out.println("could not close connection: " + e.getMessage());
-            }
-        }
-        for(User user : users) {System.out.println(user);}
     }
 }
