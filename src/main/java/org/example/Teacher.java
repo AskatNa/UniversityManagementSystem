c7eaee1b5ca7d34837863fb3d358f5ed7badfa80
Yehoshuaro <231132@astanait.edu.kz>
2024-02-15 20:57:28 +0600

    Fix the class

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -3,9 +3,8 @@ package org.example;
 public class Teacher extends User{
     private String course;
 
-    public Teacher(String name, String surname, String course){
-        setName(name);
-        setSurname(surname);
+    public Teacher(String name, String surname,int age, String course){
+        super(name,surname,age);
         setCourse(course);
     }
 
