bce266caae0d79272617dec1366a99ff3a4438a9
Nuris <230445@astanait.edu.kz>
2024-02-27 18:10:38 +0600

    I fixed main and userservice

--- src/main/java/org/example/entities/Main.java
+++ src/main/java/org/example/entities/Main.java
@@ -1,7 +1,6 @@
 package org.example.entities;
 
 import java.sql.*;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,52 +11,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    String conString = "jdbc:postgresql://localhost:5432/simpledb";
-    ResultSet resultSet = null;
-    Connection con = null;
-    Scanner scanner = new Scanner(System.in);
-    Statement stat = null;
-    List<User> users = new ArrayList<User>();
+        String conString = "jdbc:postgresql://localhost:5432/simpledb";
+        ResultSet resultSet = null;
+        Connection con = null;
+        Scanner scanner = new Scanner(System.in);
+        Statement stat = null;
+        PreparedStatement preparedStatement = null;
+        List<User> users = new ArrayList<User>();
 
-    try {
-        Class.forName("org.postgresql.Driver");
-        con = DriverManager.getConnection(conString, "postgres", "123456");
-        stat = con.createStatement();
-        resultSet = stat.executeQuery("SELECT id, name, surname, gender, age, password, course, Attendance , gpa, position, Retake FROM users ORDER BY id");
-
-        while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String name = resultSet.getString("name");
-            String surname = resultSet.getString("surname");
-            Boolean gender = resultSet.getBoolean("gender");
-            int age = resultSet.getInt("age");
-            boolean position = resultSet.getBoolean("position");
-            int Attendance = resultSet.getInt("Attendance");
-            double gpa = resultSet.getDouble("gpa");
-            boolean Retake = resultSet.getBoolean("Retake");
-            String course = resultSet.getString("course");
-            String password = resultSet.getString("password");
-            User user = User.createUser(name, surname , age, password, id, gender, course, Attendance, gpa , position,Retake);
-            users.add(user);
-            
-        }
-    } catch(ClassNotFoundException e){
-        System.out.println(e.getMessage());
-    }
-    catch(SQLException e) {
-        System.out.println(e.getMessage());
-    }
-    finally {
         try {
-            if (resultSet != null) resultSet.close();
-            if (stat != null) stat.close();
-            if (con != null) con.close();
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(conString, "postgres", "123456");
+            stat = con.createStatement();
+            resultSet = stat.executeQuery("SELECT id, name, surname, gender, age, password, course, Attendance , gpa, position, Retake FROM users ORDER BY id");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                Boolean gender = resultSet.getBoolean("gender");
+                int age = resultSet.getInt("age");
+                boolean position = resultSet.getBoolean("position");
+                int Attendance = resultSet.getInt("Attendance");
+                double gpa = resultSet.getDouble("gpa");
+                boolean Retake = resultSet.getBoolean("Retake");
+                String course = resultSet.getString("course");
+                String password = resultSet.getString("password");
+                User user = User.createUser(name, surname, age, password, id, gender, course, Attendance, gpa, position, Retake);
+                users.add(user);
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (stat != null) stat.close();
+                if (con != null) con.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
         }
-    }
+
         for (User user1 : users) {
             System.out.println(user1);
         }
+
+        // Creating an instance of UserService
+        UserService userService = new UserService();
+
+        // Calling the registration method
+        userService.registration();
     }
 }
--- src/main/java/org/example/entities/UserService.java
+++ src/main/java/org/example/entities/UserService.java
@@ -1,4 +1,4 @@
-package org.example.services;
+package org.example.entities;
 
 import java.util.Scanner;
 
